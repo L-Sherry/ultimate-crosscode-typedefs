52602a287e6693e3c12f0f4ecc1a9d628dc7f152
L Sherry <lesser.sherry@protonmail.com>
2020-07-19 19:01:32 +0200

    Add some definitions for the message overlay.

--- modules/game.feature.msg.gui.message-overlay.d.ts
+++ modules/game.feature.msg.gui.message-overlay.d.ts
@@ -1,3 +1,30 @@
 import './impact.feature.gui.gui';
 import './game.feature.msg.gui.message-box';
 import './game.feature.model.options-model';
+
+export {};
+
+declare global {
+  namespace ig {
+
+    interface MessageOverlayGui extends ig.GuiElementBase {
+    }
+    namespace MessageOverlayGui {
+      interface BlackBar extends ig.GuiElementBase {
+      }
+      interface BlackBarConstructor {
+        new() : BlackBar;
+      }
+      var BlackBar : BlackBarConstructor;
+
+      interface BottomShadow extends ig.GuiElementBase {
+      }
+      interface BottomShadowConstructor {
+              pattern: ig.ImagePattern;
+              new() : BottomShadow;
+      }
+      var BottomShadow : BottomShadowConstructor;
+    }
+
+  }
+}

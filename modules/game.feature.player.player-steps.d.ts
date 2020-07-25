2832efd375da4e9088e03060e1b7484742a89e6e
L Sherry <lesser.sherry@protonmail.com>
2020-07-25 21:21:15 +0200

    Add ACTION_STEP.SET_PLAYER_ACTION_BLOCK

--- modules/game.feature.player.player-steps.d.ts
+++ modules/game.feature.player.player-steps.d.ts
@@ -6,3 +6,24 @@ import './game.feature.combat.model.combat-params';
 import './impact.feature.camera.camera';
 import './game.feature.combat.entities.food-icon';
 import './game.feature.party.party';
+
+export {};
+
+declare global {
+  namespace ig.ACTION_STEP {
+
+    interface SET_PLAYER_ACTION_BLOCK {
+      blockData: {
+        action: number,
+        charge: number,
+        dash: number,
+        reaim: number,
+        move: number
+      }
+    }
+    interface SET_PLAYER_ACTION_BLOCK_CONSTRUCTOR {
+      new(settings: Pick<SET_PLAYER_ACTION_BLOCK, 'blockData'>): SET_PLAYER_ACTION_BLOCK
+    }
+    var SET_PLAYER_ACTION_BLOCK: SET_PLAYER_ACTION_BLOCK_CONSTRUCTOR;
+  }
+}
